f1c919984438dcacba890259a4024b83525f4c9c
Benke Zoltán <benkez@gyermelyi.hu>
2025-11-21 11:55:14 +0100

    Fix dependencies

--- setup.py
+++ setup.py
@@ -17,5 +17,11 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    install_requires=[open('requirements.txt').read().splitlines()],
+    install_requires=[
+        'bennet-observer @ git+https://github.com/zbenke71/observer.git@0.1',
+        'bennet-config @ git+https://github.com/zbenke71/config.git@0.1',
+        'oracledb>=3.4.1',
+        'pandas>=2.3.3',
+        'sqlalchemy>=2.0.44',
+    ],
 )
